09dbc99ee3312262ab5ec31e576dd553595791dd
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-04 12:55:43 +0100

    some react on rails

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -9,7 +9,7 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 # require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
@@ -21,11 +21,6 @@ module Backend
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
-    # Only loads a smaller set of middleware suitable for API only apps.
-    # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and assets when generating a new resource.
-    config.api_only = true
     config.active_job.queue_adapter = :delayed_job
   end
 end
