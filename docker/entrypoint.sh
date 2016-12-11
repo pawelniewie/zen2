91c3a2b5913ac9dcdb1292f2074856140d586db9
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-11 23:24:05 +0100

    still working on docker

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,2 +1,23 @@
 #!/bin/bash
 
+bundle check || bundle install --jobs 1 --retry 5 --with test development webkit
+
+if [ -n "$DATABASE_URL" ];then
+  COUNT=0
+  connected=1
+
+  until [[ $connected == 0 ]]; do
+    ruby /wait-for-db.rb "$DATABASE_URL"
+    connected=$?
+
+    if (( $COUNT > 10 ));then
+      echo "Could not connect to db in 10 seconds"
+      exit 1
+    fi
+    echo "Waiting for db $COUNT time"
+    ((COUNT++))
+    sleep 1
+  done
+fi
+
+exec bundle exec $@
