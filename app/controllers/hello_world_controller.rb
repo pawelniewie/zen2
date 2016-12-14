6177cbac93a83f81e2d78880afd1ee23912e39ed
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-14 22:16:43 +0100

    do not force authorization in controllers for now

--- app/controllers/hello_world_controller.rb
+++ app/controllers/hello_world_controller.rb
@@ -1,6 +1,7 @@
 class HelloWorldController < ApplicationController
 
   skip_before_action :authenticate_user!
+  skip_after_action :verify_authorized
 
   def index
     @hello_world_props = { name: "Stranger" }
--- app/controllers/root_controller.rb
+++ app/controllers/root_controller.rb
@@ -1,6 +1,7 @@
 class RootController < ApplicationController
 
   skip_before_action :authenticate_user!
+  skip_after_action :verify_authorized
 
   def index
 
