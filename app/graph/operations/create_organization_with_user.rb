670548e6d8f133c859eb9254bb6392960c0979c9
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-18 23:15:28 +0100

    mutation without complex arguments works

--- app/graph/operations/create_organization_with_user.rb
+++ app/graph/operations/create_organization_with_user.rb
@@ -1,10 +1,11 @@
 class CreateOrganizationWithUser < GraphQL::Rails::Operations
 
-  mutation create_organization_with_user: {user: UserInterface, token: String} do
+  mutation create_organization_with_user: {token: String, user: GraphQL::Rails::Types.resolve(UserInterface)} do
     description 'Creates organization with first user'
 
-    argument :organization, OrganizationInterface
-    argument :user, UserInterface
+    # argument :organization, OrganizationInterface
+    # argument :user, UserInterface, :required
+    # argument :user, String, :required
 
     resolve do
       {
