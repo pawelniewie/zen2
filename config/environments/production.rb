a9b5baeb89f93d2eee8fd1e9097833b3bbd3686a
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-18 15:37:12 +0100

    added letsencrypt heroku support

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -84,4 +84,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.middleware.insert_before ActionDispatch::SSL, Letsencrypt::Middleware
 end
