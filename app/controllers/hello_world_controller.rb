6f06145862343485f192f75db278ae8ceb0d4e52
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-21 08:27:07 +0100

    removed hello world controller

--- app/controllers/hello_world_controller.rb
+++
@@ -1,9 +0,0 @@
-class HelloWorldController < ApplicationController
-
-  skip_before_action :authenticate_user!
-  skip_after_action :verify_authorized
-
-  def index
-    @hello_world_props = { name: "Stranger" }
-  end
-end
