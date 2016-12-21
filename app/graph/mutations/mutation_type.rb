51b009851f187ad05b4ed4b97d51d376f09b551e
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-21 10:24:23 +0100

    removed createOrganizationWithUser mutation

--- app/graph/mutations/mutation_type.rb
+++ app/graph/mutations/mutation_type.rb
@@ -2,7 +2,6 @@ MutationType = GraphQL::ObjectType.define do
   name "Mutation"
   description "The mutation root for this schema"
 
-  field :createOrganizationWithUser, field: CreateOrganizationWithUserMutation.field
   field :createProject, field: CreateProjectMutation.field
   field :createIssue, field: CreateIssueMutation.field
 end
