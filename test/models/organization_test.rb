89c3f475e3355b50bb69e42ca09e2ecbf2e0254c
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-22 23:25:35 +0100

    added test for restricted slug name

--- test/models/organization_test.rb
+++ test/models/organization_test.rb
@@ -1,7 +1,14 @@
 require 'test_helper'
 
 class OrganizationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should reject restricted slug name" do
+    org = Organization.new(name: 'Test', slug: 'zen')
+    assert_not org.valid?
+    assert_equal [:slug], org.errors.keys
+  end
+
+  test "should accept slug name" do
+    org = Organization.new(name: 'Test', slug: 'another')
+    assert org.valid?
+  end
 end
