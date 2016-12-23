9002e6b07f3ba5a8060020e93b83a0de88c60f05
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-23 17:00:05 +0100

    added more reserved keywords

--- app/models/reserved_slugs.rb
+++ app/models/reserved_slugs.rb
@@ -1,5 +1,5 @@
 module ReservedSlugs
   def self.all
-    %w(test admin panel zen pawel root mx in mail dns ftp www ssh)
+    %w(test admin panel zen pawel root mx in mail dns ftp www ssh app core lukasz office incoming team)
   end
 end
