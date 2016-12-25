943cca3333c85e61f71e8b80858876a351750900
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-25 12:02:02 +0100

    added X-User-ID

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,22 @@
 class ApplicationController < ActionController::Base
   # protect_from_forgery with: :exception
+
+  after_action :add_user_headers
+
+  before_bugsnag_notify :add_user_info_to_bugsnag
+
+  protected
+
+  def add_user_info_to_bugsnag(notification)
+    notification.user = {
+      email: current_user.email,
+      id: current_user.id,
+    }
+  end
+
+  def add_user_headers
+    return unless current_user
+
+    response.headers['X-User-ID'] = current_user.id
+  end
 end
