aaa4e8185ca95ec13860debc547b44eb9e985456
Pawel Niewiadomski <11110000b@gmail.com>
2016-12-29 19:20:19 +0100

    create project mutation doesn't take organization_id

--- app/graph/mutations/create_project_mutation.rb
+++ app/graph/mutations/create_project_mutation.rb
@@ -8,16 +8,14 @@ CreateProjectMutation = GraphQL::Relay::Mutation.define do
   name "CreateProject"
 
   input_field :project, !ProjectInput
-  input_field :organizationId, types.String
 
   return_field :errors, !types[FieldErrorInterface]
   return_field :success, CreateProjectMutationSuccessInterface
 
   resolve -> (_, inputs, context) {
-    organization = Organization.find_by_id(inputs[:organizationId]) || context[:current_organization]
     project = inputs[:project].to_h
 
-    result = CreateProjectService.(project: project, organization: organization, context: context)
+    result = CreateProjectService.(project: project, organization: context[:current_organization], context: context)
     FormatMutationResultService.(result: result)
   }
 end
