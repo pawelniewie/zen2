fb54fa93da70c77390057661657c22c3158ad63c
Pawel Niewiadomski <11110000b@gmail.com>
2017-01-21 18:49:09 +0100

    moved organization spec

---
+++ spec/models/organization_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+describe Organization do
+  it "should reject restricted slug name" do
+    org = Organization.new(name: 'Test', slug: 'zen')
+
+    expect(org).to_not be_valid
+    expect(org.errors.keys).to contain_exactly :slug
+  end
+
+  it "should accept slug name" do
+    org = Organization.new(name: 'Test', slug: 'another')
+    expect(org).to be_valid
+  end
+end
