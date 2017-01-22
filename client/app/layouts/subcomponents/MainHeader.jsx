b736eeed85ad74ba1c0c3de05846410ac2b2f336
Łukasz Kużyński <wookieb>
2017-01-22 22:38:23 +0100

    User menu, dropdowns

--- client/app/layouts/subcomponents/MainHeader.jsx
+++ client/app/layouts/subcomponents/MainHeader.jsx
@@ -3,26 +3,13 @@ import InlineSVG from 'react-svg-inline';
 
 import {Link} from 'react-router';
 import SearchBar from 'app/components/SearchBar';
-import Button from 'app/components/Button';
-import Avatar from 'app/components/Avatar';
+import UserInHeader from './UserInHeader';
 
 const logo = require('!!svg-inline!app/images/logo.svg');
 
 export default function MainHeader(props) {
-    let headerUser;
-    if (props.showUserMenu) {
-        if (props.user && props.user.email) {
-            headerUser = <button className="main-header--user">
-                <span key="1" className="main-header--user--name">Paweł Niewiadomski</span>
-                <Avatar key="2" email={props.user.email} className="main-header--user--avatar"/>
-            </button>;
-        } else {
-            headerUser =
-                <Button isPrimary={true} isSmall={true} className="main-header--login" to="/user/login">Log in</Button>;
-        }
-    }
     return <header className="main-header">
-        {headerUser}
+        <UserInHeader user={props.user}/>
 
         <InlineSVG svg={logo} component={Link} to="/" className="main-header--logo"/>
         <div className="main-header--search">
