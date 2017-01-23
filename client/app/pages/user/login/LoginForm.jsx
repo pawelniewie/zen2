f8bbe45e9ca27cec55dde2aa42c747daa65ffd1f
Łukasz Kużyński <wookieb>
2017-01-23 01:19:16 +0100

    Small details

--- client/app/pages/user/login/LoginForm.jsx
+++ client/app/pages/user/login/LoginForm.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 require('./login-form.scss');
 
 import {FocusedTaskHeader, FocusedTaskContent} from 'app/layouts/FocusedTask';
-import {FormErrors, FormField} from 'app/components/Form';
+import {FormErrors, FormField, FormLabel, FormButtons} from 'app/components/Form';
 import Button from 'app/components/Button';
 
 export default class LoginForm extends React.Component {
@@ -18,22 +18,22 @@ export default class LoginForm extends React.Component {
             <FocusedTaskContent>
                 <form onSubmit={this.onSubmit} className="login-form form form__label-top">
                     <FormField>
-                        <label className="form--field--label" htmlFor="login-id">Email or login</label>
+                        <FormLabel htmlFor="login-id">Email or login</FormLabel>
                         <input type="text" ref="login" className="form--control input" id="login-id"/>
                     </FormField>
                     <FormField>
-                        <label className="form--field--label" htmlFor="password-id">Password</label>
+                        <FormLabel htmlFor="password-id">Password</FormLabel>
                         <input type="password" ref="password" className="form--control input" id="password-id"/>
                         <FormErrors errors={this.props.error && this.props.error.message}/>
                     </FormField>
 
-                    <div className="form--buttons login-form--buttons">
+                    <FormButtons className="login-form--buttons">
                         <Button className="login-form--submit"
                                 isLoading={this.isLogging}
                                 isPrimary={true}
                                 loadingLabel="Logging...">Login</Button>
                         <a href="#" className="login-form--forgot-password">Forgot your password?</a>
-                    </div>
+                    </FormButtons>
                 </form>
             </FocusedTaskContent>
         </div>;
