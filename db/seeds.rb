a82839801bccdb86c9935a6ac9bc4c184b0da950
Pawel Niewiadomski <11110000b@gmail.com>
2017-05-15 11:44:52 +0200

    make seeds.rb idempotent

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,12 +1,14 @@
-zen = Organization.create!(name: 'Zen', slug: 'zentest')
-zen.update!(slug: 'zen')
+if Organization.find_by_name('Zen').empty?
+  zen = Organization.create!(name: 'Zen', slug: 'zentest')
+  zen.update!(slug: 'zen')
 
-pawel = zen.users.create!(first_name: "Pawel", last_name: "Niewiadomski", username: "pin", password: "password", confirmed_at: Time.now, email: "11110000b@gmail.com")
+  zen.users.create!(first_name: "Pawel", last_name: "Niewiadomski", username: "pin", password: "password", confirmed_at: Time.now, email: "11110000b@gmail.com")
 
-project_a = zen.projects.create!(name: "Project A", key: "PA")
-project_b = zen.projects.create!(name: "Project B", key: "PB")
+  project_a = zen.projects.create!(name: "Project A", key: "PA")
+  project_b = zen.projects.create!(name: "Project B", key: "PB")
 
-project_a.issues.create!(summary: "This is a test issue")
-project_a.issues.create!(summary: "Please fix this before releasing")
+  project_a.issues.create!(summary: "This is a test issue")
+  project_a.issues.create!(summary: "Please fix this before releasing")
 
-project_b.issues.create!(summary: "Issue from another project")
+  project_b.issues.create!(summary: "Issue from another project")
+end
