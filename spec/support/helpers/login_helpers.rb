5592a1853a69d5268066c182fd1815832a9e5459
Pawel Niewiadomski <11110000b@gmail.com>
2017-06-11 22:20:30 +0200

    fixed race condition in login helper

--- spec/support/helpers/login_helpers.rb
+++ spec/support/helpers/login_helpers.rb
@@ -4,7 +4,6 @@ module LoginHelpers
 		Pages::LoginPage.new.load do |login_page|
 			login_page.login.set(user.email)
 			login_page.password.set('password')
-			login_page.submit.click
 			expect(login_page.login_to_default_page).to be_loaded
 		end
 	end
