382265db1722b790a015d44c897ee6c3ef5494b7
Pawel Niewiadomski <11110000b@gmail.com>
2017-06-11 18:01:24 +0200

    redirect to root without a subdomain when trying to open a tenant that doesn't exist

--- app/controllers/spa_controller.rb
+++ app/controllers/spa_controller.rb
@@ -3,6 +3,9 @@ class SpaController < ApplicationController
   layout 'spa'
 
   def index
+    if current_tenant.blank?
+      redirect_to root_url(host: request.domain, subdomain: false)
+    end
   end
 
 end
