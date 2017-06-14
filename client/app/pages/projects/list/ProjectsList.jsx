9229dd05d46c9d304ab696ebcf39141276070d1a
Pawel Niewiadomski <11110000b@gmail.com>
2017-06-14 22:27:44 +0200

    removed bootstrap

--- client/app/pages/projects/list/ProjectsList.jsx
+++ client/app/pages/projects/list/ProjectsList.jsx
@@ -3,7 +3,6 @@ import ListingLayout from 'app/layouts/ListingLayout';
 import Button from 'app/components/Button';
 import {Link} from 'react-router';
 
-import {Nav, NavItem} from 'react-bootstrap';
 require('./projects-list.scss');
 
 export default function ProjectsList({projects}) {
