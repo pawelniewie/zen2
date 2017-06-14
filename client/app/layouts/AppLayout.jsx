697ed6c59d8948316c33fdb0182a747568f3f79f
Pawel Niewiadomski <11110000b@gmail.com>
2017-06-14 23:37:53 +0200

    switched top menu to ant design

--- client/app/layouts/AppLayout.jsx
+++ client/app/layouts/AppLayout.jsx
@@ -1,9 +1,33 @@
 import React from 'react';
-import MainHeader from './subcomponents/MainHeader';
+import { Menu, Icon, Button } from 'antd';
+import InlineSVG from 'react-svg-inline';
+import {Link} from 'react-router';
+
+const SubMenu = Menu.SubMenu;
+
+const logo = require('!!svg-inline-loader!app/images/logo.svg');
+
+require('./AppLayout.scss');
 
 export default function AppLayout(props) {
     return <div>
-        <MainHeader user={props.user} showUserMenu={!props.isLoading}/>
+        <Menu mode="horizontal" className="top-menu">
+            <Menu.Item>
+                <InlineSVG svg={logo} component={Link} to="/" className="main-header--logo"/>
+            </Menu.Item>
+            {props.user !== undefined ? (
+                <SubMenu title={<span><Icon type="user" />{props.user.first_name} {props.user.last_name}</span>}>
+                    <Menu.Item key="user:logout">Log out</Menu.Item>
+                </SubMenu>
+            ) : (
+                <Menu.Item>
+                    <Button type="primary" size="small" icon="user">
+                        <Link to="/users/login" className="log-in">Log in</Link>
+                    </Button>
+                </Menu.Item>
+            )}
+        </Menu>
+
         <main className="main-content">
             {props.children}
         </main>
