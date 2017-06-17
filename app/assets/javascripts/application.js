1431924bbde0707fbd39066a70fa741a6dcda713
Paweł Pawłowski <pavvelpe@gmail.com>
2017-06-17 02:58:02 +0200

    required stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,2 +1,12 @@
-//= require webpack-bundle
+//= require reactBoilerplateDeps.dll.js
+//= require webpack-bundle.js
+//= require 0.webpack-bundle.js
+//= require 1.webpack-bundle.js
+//= require 2.webpack-bundle.js
+//= require 3.webpack-bundle.js
+//= require 4.webpack-bundle.js
+//= require 5.webpack-bundle.js
+//= require 6.webpack-bundle.js
+//= require 7.webpack-bundle.js
+
 
