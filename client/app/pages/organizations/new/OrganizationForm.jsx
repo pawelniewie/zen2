084fb2cf3e205fdd8d44042f5617b73422111206
Pawel Niewiadomski <11110000b@gmail.com>
2017-06-30 23:08:29 +0200

    more fields on organization page

--- client/app/pages/organizations/new/OrganizationForm.jsx
+++ client/app/pages/organizations/new/OrganizationForm.jsx
@@ -1,27 +1,44 @@
 import React from 'react';
 import { Form, Icon, Input } from 'semantic-ui-react';
 import { Field } from 'redux-form';
+import FocusedTask, { FocusedTaskContent } from 'app/layouts/FocusedTask';
 
 require('./OrganizationForm.scss');
-
-import {FocusedTaskHeader, FocusedTaskContent} from 'app/layouts/FocusedTask';
-import {FormErrors, FormField, FormLabel, FormButtons} from 'app/components/Form';
-import Button from 'app/components/Button';
 
 export default class OrganizationForm extends React.Component {
     constructor(props) {
         super(props);
-
         this.onSubmit = this.onSubmit.bind(this)
     }
 
     render() {
-        return <Form onSubmit={this.onSubmit}>
-            <Form.Field>
-                <label>Organisation name</label>
-                <Input placeholder="Put a name here"/>
-            </Form.Field>
-        </Form>;
+        return <FocusedTask>
+            <FocusedTaskContent>
+                <Form onSubmit={this.onSubmit}>
+                    <Form.Field>
+                        <label>Organisation name</label>
+                        <Field name="name" component={Input} placeholder="Put a name here"/>
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Subdomain</label>
+                        <Field name="slug" component={Input} placeholder="Your subdomain"/>
+                    </Form.Field>
+                    <Form.Field>
+                        <label>E-mail</label>
+                        <Field name="email" component={Input} placeholder="Your business e-mail"/>
+                    </Form.Field>
+                    <Form.Field>
+                        <label>First name</label>
+                        <Field name="first_name" component={Input} placeholder="First name"/>
+                    </Form.Field>
+                    <Form.Field>
+                        <label>Last name</label>
+                        <Field name="last_name" component={Input} placeholder="Last name"/>
+                    </Form.Field>
+                    <Form.Button primary disabled={this.props.pristine || this.props.submitting}>Create</Form.Button>
+                </Form>
+            </FocusedTaskContent>
+        </FocusedTask>;
     }
 
     onSubmit(e) {
