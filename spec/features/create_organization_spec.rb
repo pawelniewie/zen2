65e29bf868098def1878e6edf1dd988a95cf1d00
Pawel Niewiadomski <11110000b@gmail.com>
2017-07-22 12:21:46 +0200

    fixed spec for organizations new

--- spec/features/create_organization_spec.rb
+++ spec/features/create_organization_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 RSpec.feature 'creating an organization' do
-	xit 'works' do
-		Pages::CreateOrganization.new.load do |page|
-			page.organization_name.set('Another')
-			page.organization_slug.set('another')
-			projects_page = page.create_organization
-			expect(projects_page).to be_loaded
-		end
+	it 'works' do
+		expect do
+			Pages::CreateOrganization.new.load do |page|
+				page.organization_name.set('Another')
+				page.organization_slug.set('another')
+				
+				page.first_name.set('Pawel')
+				page.last_name.set('Smith')
+				page.password.set('password')
+				page.email.set('no@such.email')
+				
+				projects_page = page.create_organization
+				expect(projects_page).to be_loaded
+			end
+		end.to change { ActionMailer::Base.deliveries.size }.by(1)
 	end
 end
