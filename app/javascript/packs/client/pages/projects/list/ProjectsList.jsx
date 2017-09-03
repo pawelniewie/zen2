7034c9aca387b1e9dffede4cc8eb8f208f507569
Pawel Pawlowski <pavvelpe@gmail.com>
2017-09-03 17:20:14 +0200

    project list love

--- app/javascript/packs/client/pages/projects/list/ProjectsList.jsx
+++ app/javascript/packs/client/pages/projects/list/ProjectsList.jsx
@@ -12,15 +12,25 @@ export default function ProjectsList({projects}) {
         <table className="table projects-list">
             <thead>
             <tr>
-                <th className="projects-list--key">Key</th>
-                <th className="projects-list--name">Project name</th>
+                <th className="projects-list--header item-header--key">Key</th>
+                <th className="projects-list--header item-header--name">Project name</th>
+								<th className="projects-list--header item-header--members">Members</th>
+								<th className="projects-list--header item-header--tasks-assigned">Assigned tasks</th>
+								<th className="projects-list--header item-header--tasks-in-progress">In progress</th>
+								<th className="projects-list--header item-header--tasks-completed">Completed</th>
+								<th className="projects-list--header item-header--project-owner">Project Owner</th>
             </tr>
             </thead>
             <tbody>
             {(projects || []).map(project => (
                 <tr key={project.key}>
-                    <td className="projects-list--key">{project.key}</td>
-                    <td className="projects-list--name"><Link to={`/projects/${project.key}`}>{project.name}</Link></td>
+                    <td className="projects-list--item">{project.key}</td>
+                    <td className="projects-list--item"><Link to={`/projects/${project.key}`}>{project.name}</Link></td>
+										<td className="projects-list--item">{Math.floor(Math.random() *25 + 2)}</td>
+										<td className="projects-list--item">{Math.floor(Math.random() *25 + 2)}</td>
+										<td className="projects-list--item">{Math.floor(Math.random() *55 + 2)}</td>
+										<td className="projects-list--item">{Math.floor(Math.random() *95 + 2)}</td>
+										<td className="projects-list--item">Pawel {Math.floor(Math.random() *9 + 2)} </td>
                 </tr>
             ))}
             </tbody>
